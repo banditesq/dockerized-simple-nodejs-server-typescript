3ebaee97b6baf5611a28a2b99a374aebc9d28998
bandittu <109623633+banditesq@users.noreply.github.com>
2022-09-08 16:22:11 +0100

    add test

--- app/api/user_actions/actions.ts
+++ app/api/user_actions/actions.ts
@@ -1,25 +1,20 @@
 import express from "express";
 import { execFunction } from "../../utils/user_traits/traits";
 
-
-const router = express.Router();
-
+const router = express();
 
 router.get("/details", async (req: any, res) => {
-    const { _id } = req.decoded
+  const { _id } = req.decoded;
 
-    try {
-        await execFunction(async auth => {
-            const details = await auth.details({ _id })
+  try {
+    await execFunction(async (auth) => {
+      const details = await auth.details({ _id });
 
-            res.status(200).json(details)
-        })
-    } catch (error) {
-        res.status(400).send(`could not get user: ${error}`)
-
-    }
-})
-
-
+      res.status(200).json(details);
+    });
+  } catch (error) {
+    res.status(400).send(`could not get user: ${error}`);
+  }
+});
 
 export default router;
--- app/index.ts
+++ app/index.ts
@@ -1,33 +1,31 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
 
-import user_auth from "./api/user_auth/user_auth"
+import user_auth from "./api/user_auth/user_auth";
 
 import user_action from "./api/user_actions/actions";
 
-import {auth_middleware} from "./routes_protection/index";
+import { auth_middleware } from "./routes_protection/index";
 
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
 
 import mongoose from "mongoose";
-
-
+import http from "http";
 
 const app = express();
 
-const port = process.env.SERVER_PORT || 8002
-
-
-
-app.use(bodyParser.urlencoded({extended:true}))
+const port = process.env.SERVER_PORT || 8002;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URI!);
 
-app.use("/user_auth",user_auth)
-app.use("/user", auth_middleware,user_action)
+app.use("/user_auth", user_auth);
+app.use("/user", auth_middleware, user_action);
 
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
 
-app.listen(port,()=>{
-    console.log(`server running on port ${port} `)
-    
-})
+export default server;
